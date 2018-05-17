937f7ba0bbf618f7ee03745bdba171d8a48a7412
Sanjana Jain <sanjanajain@sanjanas-mbp.home>
2018-05-17 01:07:10 +0100

    Scenarios added

---
+++ src/test/java/com/test/Hooks.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void beforeStartup(){
+
+        System.setProperty("webdriver.gecko.driver", "/Users/sanjanajain/rbs/src/drivers/node_modules/geckodriver/geckodriver");
+        //file:///Users/sanjanajain/rbs/src/drivers/chromedriver
+        ///Users/sanjanajain/rbs/src/drivers/node_modules/chromedriver/bin/chromedriver
+        driver = new FirefoxDriver();
+        driver.get("http://automationpractice.com/index.php");
+
+    }
+
+    //@After
+    //public void afterScenario(){
+    //driver.quit();
+    //}
+}
---
+++ src/test/java/com/test/stepDefs.java
@@ -0,0 +1,64 @@
+package com.test;
+
+import com.test.PageObjects.homepage;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
+public class stepDefs{
+    WebDriver driver = Hooks.driver;
+    WebDriverWait wait = new WebDriverWait(driver,10);
+    SoftAssert softAssert = new SoftAssert();
+
+    @Given ("^User is in T-shirt catalog page$")
+    public void loggedInUser() {
+        homepage page = new homepage(driver);
+        page.clickTshirtButton();
+        driver.findElement(By.cssSelector("#block_top_menu > ul > li:nth-child(3) > a")).click();
+    }
+
+    @When("^user order a T-shirt$")
+    public void orderTshirt() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#center_column > ul > li > div > div.left-block > div > a.product_img_link > img")));
+        driver.findElement(By.cssSelector("#center_column > ul > li > div > div.left-block > div > a.product_img_link > img")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#add_to_cart > button > span")));
+        //driver.findElement(By.id("add_to_cart")).click();
+        driver.findElement(By.cssSelector("#add_to_cart > button > span")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span")));
+        driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span")));
+        driver.findElement(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("someone@example.com");
+        driver.findElement(By.cssSelector("#passwd")).sendKeys("Password123");
+        driver.findElement(By.cssSelector("#SubmitLogin > span")).click();
+        driver.findElement(By.cssSelector("button.button:nth-child(4)")).click();
+        driver.findElement(By.cssSelector("#cgv")).click();
+        driver.findElement(By.cssSelector("button.button:nth-child(4)")).click();
+        driver.findElement(By.cssSelector(".bankwire")).click();
+        driver.findElement(By.cssSelector("button.button-medium")).click();
+        String text = driver.findElement(By.cssSelector(".box")).getText();
+        String verfication[] =text.split(" ");
+        String orderNumber = verfication[36];
+    }
+
+
+    @Then("^user should have those details in order history$")
+    public void orderhistory(){
+
+        driver.findElement(By.cssSelector(".account > span:nth-child(1)")).click();
+        driver.findElement(By.cssSelector("div.col-sm-6:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > span:nth-child(2)")).click();
+        String OrderNumber = driver.findElement(By.cssSelector("tr.first_item > td:nth-child(1) > a:nth-child(2)")).getText();
+        System.out.println("Order Number="+OrderNumber);
+        System.out.println(test);
+        //softAssert.assertEquals(OrderNumber,orderTshirt());
+
+
+
+    }
+}
